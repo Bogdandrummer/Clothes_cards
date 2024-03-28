1ef50d5c02bde49daa4076d8679234d5455e1b5f
Bogdan <104357137+Bogdandrummer@users.noreply.github.com>
2024-03-28 10:35:22 +0200

    Added variables

--- js/index.js
+++ js/index.js
@@ -1,13 +1,25 @@
 const cardsList = document.querySelector('data-cards_list');
+const cardTitle = document.querySelector('data-card_title');
+console.log(cardTitle);
 
 
+//Creating variables of card
+const createCards = (card) => {
+	const cardsList = document.querySelector('data-cards-list');
+	const cardsTemplate = document.querySelector('[data-type="card-template"]');
+	const newCard = document.importNode(cardTemplate.content, true);
+	newCard.document.querySelector('data-card_img').src = card.img;
+	newCard.document.querySelector('data-card_title').innerText = card.title;
+	newCard.document.querySelector('data-card-description').innerText = card.description;
+	newCard.document.querySelector('data-card-price').innerText = card.price;
+	
+	}
 
 async function Recieve() {
 let response = await fetch('https://api.escuelajs.co/api/v1/products');
 if (response.ok) {
 	let base = await response.json();
-	console.log(base[5]);
-	
+	createCards(base);
 	return base;
 }
 
