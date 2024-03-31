70ee2f5b96e9ded3ddabf9e37a5364a059896b22
bogdandrummer <bogdandrummer64@gmail.com>
2024-03-31 13:56:16 +0300

    Cards already can render

--- js/index.js
+++ js/index.js
@@ -1,17 +1,33 @@
 const cardsList = document.querySelector('[data-cards_list]');
-console.log(cardsList);
+
 let clothes = [];
+const dot = '...';
+
 
 
 //Creating variables of card
 const createCards = (card) => {
 	const cardsTemplate = document.querySelector('[data-type="card-template"]');
 	const newCard = document.importNode(cardsTemplate.content, true);
-	newCard.querySelector('[data-card_img]').src = card.images[0];
-	newCard.querySelector('[data-card_title]').innerText = card.title;
-	newCard.querySelector('[data-card-description]').innerText = card.description;
-	newCard.querySelector('[data-card-price]').innerText = card.price;
 	
+	newCard.querySelector('[data-card_img]').src = 'https://placehold.co/600x400';
+	newCard.querySelector('[data-card_title]').innerText = card.title;
+	newCard.querySelector('[data-card_description]').innerText = card.description;
+	newCard.querySelector('[data-card_price]').innerText = card.price;
+	
+	const cutDescription = newCard.querySelector('[data-card_description]').innerText;
+	
+	if (cutDescription.length > 300) {
+		newCard.querySelector('[data-card_description]').innerText = `${cutDescription.slice(0, 300)}${dot}`;	
+	}
+	const cutDesc =  () => {
+		if (cutDescription.length > 300) {
+			newCard.querySelector('[data-card_description]').innerText = card.description;
+		} else {
+			newCard.querySelector('[data-card_description]').innerText = `${cutDescription.slice(0, 300)}${dot}`;
+		}
+		}
+		
 	return newCard;	
 	}
 
@@ -20,25 +36,18 @@ const renderCards = (container, cardsCount) => {
 	const fragment = document.createDocumentFragment();
 	cardsCount.forEach((card) => {
 		const cardItem = createCards(card);
-		console.log(card.price.innerText);
+		console.log(cardItem);
 		fragment.appendChild(cardItem);
   });
   container.appendChild(fragment);
   return container;
 };
 
-renderCards(cardsList, clothes);
-
-
-
 async function Recieve() {
 let response = await fetch('https://api.escuelajs.co/api/v1/products');
 if (response.ok) {
-	// let base = await response.json();
 	clothes = await response.json();
-	console.log(clothes[0].description);
-	
-	renderCards(cardsList, clothes.slice(0, 20));
+	renderCards(cardsList, clothes);
 	return clothes;
 }
 
@@ -53,7 +62,7 @@ return clothes;
 
 const init = () => {
 	Recieve();
-}
+	}
 
 window.addEventListener('load', init);
 
